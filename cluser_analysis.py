86444ed4528dccfd6f0a15ec56cf5dcc19ff74fc
shapiraoz <shapiraoz@gmail.com>
2013-10-06 05:30:03 +0000

    * maybe fix bug * add 3 strong values .

--- cluser_analysis.py
+++ cluser_analysis.py
@@ -5,6 +5,8 @@ import os
 from utils import SaveStatistics2File
 import utils
 
+
+
 dendorgram = None
 
 CSV_COMMUNITIES_SIZE_FILE="communitiesSize.csv"
@@ -75,18 +77,48 @@ def AvgMinMaxList(list):
     maxlist = max(list)
     return avg,minList,maxlist    
     
+    
+def GetEdgeNodeName(edge2weightList,graph,index):    
+    name1a = utils.GetNodeName(edge2weightList[index][0][0],graph)
+    name1b = utils.GetNodeName(edge2weightList[index][0][1],graph)
+    returnStr = "%s__2__%s"%(name1a,name1b)
+    return returnStr
+    #return   
+      
+def StrongestEdgesItems(edge2weightList,graph):
+    name1= GetEdgeNodeName(edge2weightList,graph,0)
+    name2= GetEdgeNodeName(edge2weightList,graph,1)
+    name3= GetEdgeNodeName(edge2weightList,graph,2)
+    return name1,name2,name3
+    
 def FindEdges(nodeList,graph):
-    edgeList =[]
+    edgeList =[] #sort list
+   
     for edge in graph.edges_iter():
         weight = graph[edge[0]][edge[1]]['weight']
-        if edge[0] in nodeList and edge[1] in nodeList and weight > SIGNIFICAT_WEIGHT: 
-            edgeList.append(edge)
+        name1 = utils.GetNodeName(edge[0],graph)
+        name2 = utils.GetNodeName(edge[1],graph)
+        if edge[0] in nodeList and edge[1] in nodeList and weight > SIGNIFICAT_WEIGHT and name1 != name2: 
+            #insert to sort list
+            item=[edge,weight]
+            if len(edgeList) == 0:
+                edgeList.append(item)
+                continue
+            index=0 
+            for runItem in edgeList :
+                if runItem[1] < weight :
+                    break
+                index=index+1
+            edgeList.insert(index, item)
+                                
+            #edgeList[weight]=edge # can be problem if there is the same weight 
+    
     return edgeList    
     
-def GetWeightEdge(edgeList,graph):
+def GetWeightEdge(edge2weightList):
     weightList=[]
-    for edge in edgeList:
-        weight = graph[edge[0]][edge[1]]['weight'] 
+    for edge in edge2weightList:
+        weight = edge[1]
         if (weight!= None):
             weightList.append(weight)
     return weightList
@@ -100,14 +132,14 @@ def RunClusterStatistics(graph):
         os.remove(CSV_GROUP_SUM)
     csvFile = open(CSV_GROUP_SUM,"wb")
     writer = csv.writer(csvFile,delimiter=",",quotechar='\n', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['group number','number of members', 'minimum Degree','Average Degree','maximum Degree','minimum weight','Average weight','maximum weight'])
+    writer.writerow(['group number','number of subjects', 'minimum Degree','Average Degree','maximum Degree','minimum weight','Average weight','maximum weight', 'the strongest edge','second edge','third edge'])
     if comMemClean==None or len(comMemClean)==0:
         LogPrint( "error cluster was not init !!!" )
         return  
     LogPrint( "size of comMemClean= %d "% len(comMemClean))
     for group in comMemClean.iteritems():
         maxDeg =0
-        minDeg =1000000
+        minDeg =10000000
         maxWeight = -1
         minWeight = -1
         numMembers = len(group[1])
@@ -131,10 +163,12 @@ def RunClusterStatistics(graph):
             LogPrint("group don't have edges....")
         else:
             LogPrint ("number edges=%d" % edgeListLen)
-            WeightList = GetWeightEdge(edgeList,graph)
+            WeightList = GetWeightEdge(edgeList)
             avgWeight,minWeight,maxWeight = AvgMinMaxList(WeightList)
             LogPrint("avg weight is %f" % avgWeight  )
-        writer.writerow([group[0],numMembers,minDeg,avgDeg,maxDeg,minWeight,avgWeight,maxWeight])
+        name1,name2,name3 = StrongestEdgesItems(edgeList, graph)
+        LogPrint( "names for insert :%s,%s,%s"% (name1,name2,name3))
+        writer.writerow([group[0],numMembers,minDeg,avgDeg,maxDeg,minWeight,avgWeight,maxWeight,name1,name2,name3])
     LogPrint("Done!!!")
     csvFile.close()
         
